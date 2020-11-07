0aba36bf2721f8bf901121dbf4eec838fae5dacf
Kadu Lemos <kadulemos@hotmail.com>
2020-11-07 15:03:28 -0300

    Update Exercício3Vetor.java

--- Aula2/src/br/com/aula2/Exercício3Vetor.java
+++ Aula2/src/br/com/aula2/Exercício3Vetor.java
@@ -4,17 +4,21 @@ import javax.swing.JOptionPane;
 
 public class Exercício3Vetor {
 
-
 	public static void main(String[] args) {
 
-		int[] vetor = new int[4];
+		int contador = Integer.valueOf(JOptionPane.showInputDialog(null, "Qual o número de elementos do vetor?"));
+		int[] vetor = new int[contador];
+		int elemento1 = 0;
+		int ultimoElemento = contador - 1;
 
 		for (int i = 0; i < vetor.length; i++) {
-			vetor[i]++;
-			System.out.println(vetor[i]);
-			
+			vetor[i] = Integer.valueOf(JOptionPane.showInputDialog(null, "Digite o elemento da posição " + (i+1) + " :"));
+			System.out.println("Vetor " + i + " :" + vetor[i]);
 		}
-		//JOptionPane.showMessageDialog(null, "A soma dos valores do vetor com " + contador + " índices é: " + soma);
+		elemento1 = vetor[0];
+		vetor[0] = vetor[ultimoElemento];
+		System.out.println("O primeiro elemento que era " + elemento1 + " passa a valer: " + vetor[0]);
+		//JOptionPane.showMessageDialog(null, "");
 	}
 
 }
