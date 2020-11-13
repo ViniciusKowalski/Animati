9670d570334d12018ced710b0ef566be8b1c1d93
Kadu Lemos <kadulemos@hotmail.com>
2020-11-13 13:11:39 -0300

    Aula 5 | enum ConselhoProfissionalType

--- Aula5/ProjetoAula5/src/main/java/br/com/animati/ConselhoProfissionalType.java
+++ Aula5/ProjetoAula5/src/main/java/br/com/animati/ConselhoProfissionalType.java
@@ -2,4 +2,17 @@ package br.com.animati;
 
 public enum ConselhoProfissionalType {
 
+	CNS ("Cartão Nacional de Saúde"),
+	CRM ("Conselho Regional de Medicina");
+	
+	private String descricao;
+	
+	ConselhoProfissionalType(String descricao) {
+		this.descricao = descricao;	
+		}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
 }
